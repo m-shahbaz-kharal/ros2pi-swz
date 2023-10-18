88acee55f457d0ab5d18f1c2947f2898fc71c8f9
m-shahbaz-kharal <M.shahbaz.kharal@outlook.com>
2023-10-18 18:44:54 -0400

    cleaned up the script

--- install_all.sh
+++ install_all.sh
@@ -4,17 +4,16 @@
 echo "#########################################################"
 echo "INSTALLING ROS2 HUMBLE"
 echo "#########################################################"
-sudo apt update && sudo apt install locales -y
+sudo apt update && sudo apt upgrade -y
+sudo apt install locales -y
 sudo locale-gen en_US en_US.UTF-8
 sudo update-locale LC_ALL=en_US.UTF-8 LANG=en_US.UTF-8
 export LANG=en_US.UTF-8
 sudo apt install software-properties-common -y
 sudo add-apt-repository universe -y
-sudo apt update && sudo apt install curl -y
+sudo apt install curl -y
 sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(. /etc/os-release && echo $UBUNTU_CODENAME) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
-sudo apt update -y
-sudo apt upgrade -y
 sudo apt install ros-humble-ros-base -y
 sudo apt install python3-colcon-common-extensions -y
 sudo apt install python3-rosdep2 -y
