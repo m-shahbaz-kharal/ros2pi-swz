d884257499ebb5c25de4f4b2e00adfb034ac161f
m-shahbaz-kharal <M.shahbaz.kharal@outlook.com>
2023-10-20 19:08:23 -0400

    added echo messages to start_capture.sh

--- start_capture.sh
+++ start_capture.sh
@@ -1,7 +1,25 @@
 #!/bin/bash
+echo "#########################################################"
+echo "SETTING ETHERNET MTU 9000"
+echo "#########################################################"
 sudo ip link set dev eth0 mtu 9000
+
+echo "#########################################################"
+echo "Starting CAMERA DRIVER"
+echo "#########################################################"
 source flir_ws/install/local_setup.bash && ros2 launch spinnaker_camera_driver driver_node.launch.py camera_type:=blackfly_s serial:="'23422874'" &
+
 sleep 10
+echo "#########################################################"
+echo "STARTING LIDAR DRIVER"
+echo "#########################################################"
 source ouster_ws/install/local_setup.bash && ros2 launch ouster_ros sensor.launch.xml sensor_hostname:=192.168.1.12 viz:=false proc_mask:="IMG|PCL|IMU" timestamp_mode:=TIME_FROM_ROS_TIME use_system_default_qos:=true ouster_ns:=lidar &
+
+# or
+# source hesai_ws/install/local_setup.bash && ros2 launch hesai_ros_driver start.py &
+
 sleep 10
+echo "#########################################################"
+echo "STARTING DATA CAPTURE"
+echo "#########################################################"
 ros2 bag record -o /mnt/output /flir_camera/camera_info /flir_camera/image_raw /lidar/points
